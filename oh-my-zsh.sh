776ffa86583d8593ef00e8ed0091d7646b34f0b5
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-04-01 15:14:26 +0300

    Fix the lib and custom glob-sourcings

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -5,10 +5,20 @@ fpath=($ZSH/functions $fpath)
 
 # Load all of the config files in ~/oh-my-zsh that end in .zsh
 # TIP: Add files you don't want in git to .gitignore
-for config_file ($ZSH/lib/*.zsh) source $config_file
+if [[ -d $ZSH/lib ]]; then
+	for config_file in $ZSH/lib/*.zsh
+	do
+		source $config_file
+	done
+fi
 
 # Load all of your custom configurations from custom/
-for config_file ($ZSH/custom/*.zsh) source $config_file
+if [[ -d $ZSH/custom ]]; then
+	for config_file in $ZSH/custom/*.zsh
+	do
+		source $config_file
+	done
+fi
 
 # Load all of the plugins that were defined in ~/.zshrc
 plugin=${plugin:=()}
