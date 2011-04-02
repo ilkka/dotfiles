b99178e7d8103f0bcddb391f01e1641c1e6bf6c5
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-04-02 14:40:25 +0300

    Source stuff from ~/.zshrc-local and ~/.zshrc.d/*.zsh

--- lib/homedir-bin-in-path.zsh
+++ lib/homedir-bin-in-path.zsh
@@ -6,3 +6,16 @@ then
 	fi
 fi
 
+# lastly, read local settings from ~/.zshrc-local and ~/.zshrc.d/*.zsh
+localsettings=$HOME/.zshrc-local
+if [[ -f $localsettings && -r $localsettings ]]; then
+	source $localsettings
+fi
+
+localsettingsdir=$HOME/.zshrc.d
+if [[ -d $localsettingsdir ]]; then
+	for configfile in $localsettingsdir/*.zsh; do
+		source $configfile
+	done
+fi
+
