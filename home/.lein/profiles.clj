b57d69b741eca7afd6eed563036a96c81b66f58c
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2013-12-01 20:05:07 +0200

    Create profiles.clj

---
+++ home/.lein/profiles.clj
@@ -0,0 +1,4 @@
+{:user {:plugins [[lein-difftest "2.0.0"]
+                  [heroku/lein-template "0.2.1"]
+                  [compojure/lein-template "0.3.2"]
+                  [lein-midje "3.1.3"]]}}
