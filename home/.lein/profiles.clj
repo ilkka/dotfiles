e0f17cf3d76cd42c1bbbd8a628ee9bc19664b101
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2013-12-07 14:29:23 +0200

    Add lein-joodo plugin

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-difftest "2.0.0"]
                   [heroku/lein-template "0.2.1"]
                   [compojure/lein-template "0.3.2"]
-                  [lein-midje "3.1.3"]]}}
+                  [lein-midje "3.1.3"]
+                  [joodo/lein-joodo "2.0.0"]]}}
