591df5442f5ec91cb30211a6232a1290701cc738
ilkka.s.laukkanen@gmail.com <ilkka.s.laukkanen@gmail.com>
2014-08-15 09:59:20 +0300

    Add lein-autoreload

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -3,4 +3,5 @@
                   [compojure/lein-template "0.3.2"]
                   [lein-midje "3.1.3"]
                   [joodo/lein-joodo "1.2.0"]
-                  [jonase/eastwood "0.1.0"]]}}
+                  [jonase/eastwood "0.1.0"]
+                  [lein-autoreload "0.1.0"]]}}
