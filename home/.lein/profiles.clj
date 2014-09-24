9eff06ceab4187779070253c41c1345c9351925f
ilkka.s.laukkanen@gmail.com <ilkka.s.laukkanen@gmail.com>
2014-09-24 13:35:37 +0300

    Drop lein plugins I don't need

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,6 +1,4 @@
 {:user {:plugins [[lein-difftest "2.0.0"]
-                  [heroku/lein-template "0.2.1"]
-                  [compojure/lein-template "0.3.2"]
                   [lein-midje "3.1.3"]
                   [joodo/lein-joodo "1.2.0"]
                   [jonase/eastwood "0.1.0"]
