6b6db0cee23271630a67d8fa19762b9629726ec2
Ilkka Laukkanen <ilkka.laukkanen@futurice.com>
2015-06-18 12:16:34 +0300

    Update lein plugins

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[lein-difftest "2.0.0"]
                   [lein-midje "3.1.3"]
                   [joodo/lein-joodo "1.2.0"]
-                  [jonase/eastwood "0.1.0"]
+                  [jonase/eastwood "0.2.1"]
                   [lein-autoreload "0.1.0"]
-                  [cider/cider-nrepl "0.7.0"]]}}
+                  [cider/cider-nrepl "0.9.0"]]}}
