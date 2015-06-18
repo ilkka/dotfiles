ee8053dee7f3e5848587cbf93095589bbeee2805
Ilkka Laukkanen <ilkka.laukkanen@futurice.com>
2015-06-18 11:54:31 +0300

    Set up ensime for scala

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -126,3 +126,8 @@
 ;; *******************************************
 ;; editorconfig
 (load "editorconfig")
+
+;; *******************************************
+;; ensime for scala
+(require 'ensime)
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
