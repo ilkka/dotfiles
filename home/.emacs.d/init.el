8a9a52bb3441c237fd2ec6680269807713e9d36b
Ilkka Laukkanen <ilkka.laukkanen@futurice.com>
2015-06-18 11:05:01 +0300

    Set up emacs with help of starter kit instructions
    Gonna go all manual this time.

---
+++ home/.emacs.d/init.el
@@ -0,0 +1,102 @@
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-emacsclient-executable "/usr/local/bin/emacsclient")
+ '(ns-right-alternate-modifier (quote none)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; *******************************************
+;; install packages
+(defvar install-these '(better-defaults
+			paredit
+			idle-highlight-mode
+			ido-ubiquitous
+			find-file-in-project
+			magit
+			smex
+                        swiper
+                        multiple-cursors
+                        expand-region
+                        editorconfig))
+
+(package-initialize)
+(dolist (p install-these)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; *******************************************
+;; set up magit
+(setq magit-last-seen-setup-instructions "1.4.0")
+(setq magit-repo-dirs '("~/src"))
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; *******************************************
+;; set up smex
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; to use normal M-x:
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; *******************************************
+;; set up ido-ubiquitous
+;; enable ido
+(ido-mode 1)
+(ido-everywhere 1)
+;; ido for org-mode and magit
+(setq org-completion-use-ido t)
+(setq magit-completing-read-functon 'magit-ido-completing-read)
+;; ido-ubiquitous itself
+(ido-ubiquitous-mode 1)
+
+;; *******************************************
+;; basic paredit
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+;; Stop SLIME's REPL from grabbing DEL,
+;; which is annoying when backspacing over a '('
+(defun override-slime-repl-bindings-with-paredit ()
+  (define-key slime-repl-mode-map
+    (read-kbd-macro paredit-backward-delete-key) nil))
+(add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
+
+;; *******************************************
+;; swiper
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "C-r") 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+
+;; *******************************************
+;; multiple cursors
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-c C->") 'mc/mark-more-like-this-extended)
+
+;; *******************************************
+;; expand region
+(require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+;; *******************************************
+;; editorconfig
+(load "editorconfig")
