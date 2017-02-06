4d5b7fe3dbaea41420ada69a34abc1a95e086d1f
Ilkka Laukkanen <ilkka@ilkka.io>
2017-02-06 07:33:15 +0200

    fundle -> fisherman

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,7 +1,1 @@
-fundle plugin 'tuvistavie/fish-ssh-agent'
-fundle plugin 'edc/bass'
-
-fundle init
-
-test -r /usr/share/autojump/autojump.fish; and source /usr/share/autojump/autojump.fish
 test -r $HOME/.homesick/repos/homeshick/homeshick.fish; and source $HOME/.homesick/repos/homeshick/homeshick.fish
