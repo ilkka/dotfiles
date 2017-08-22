11933631e22cbb5d9cd55d20f3e03abd949579ef
Ilkka Poutanen <ilkka@ilkka.io>
2017-08-22 15:24:01 +0300

    Pass args into glout/glin

--- home/.config/fish/functions/glin.fish
+++ home/.config/fish/functions/glin.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/s7/2ypm2j2j7m1_71jb9wx02tsh0000gq/T//fish.5b2Asb/glin.fish @ line 1
+# Defined in - @ line 2
 function glin
-	git log --oneline --decorate --graph HEAD..(git for-each-ref --format='%(upstream:short)' (git symbolic-ref -q HEAD))
+	git log --oneline --decorate --graph $argv HEAD..(git for-each-ref --format='%(upstream:short)' (git symbolic-ref -q HEAD))
 end
--- home/.config/fish/functions/glout.fish
+++ home/.config/fish/functions/glout.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/s7/2ypm2j2j7m1_71jb9wx02tsh0000gq/T//fish.pSZiiO/glout.fish @ line 2
+# Defined in - @ line 2
 function glout
-	git log --oneline --decorate --graph (git for-each-ref --format='%(upstream:short)' (git symbolic-ref -q HEAD))..HEAD
+	git log --oneline --decorate --graph $argv (git for-each-ref --format='%(upstream:short)' (git symbolic-ref -q HEAD))..HEAD
 end
