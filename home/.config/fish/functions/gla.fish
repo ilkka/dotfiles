9b12f69dcf97c2bde310d92d4431b2c9a9035bba
Ilkka Poutanen <ilkka@ilkka.io>
2017-08-22 14:49:49 +0300

    Pass args to git log aliases

--- home/.config/fish/functions/gla.fish
+++ home/.config/fish/functions/gla.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/s7/2ypm2j2j7m1_71jb9wx02tsh0000gq/T//fish.BwShik/gla.fish @ line 1
+# Defined in /var/folders/s7/2ypm2j2j7m1_71jb9wx02tsh0000gq/T//fish.JABgCg/gla.fish @ line 2
 function gla
-	git log --oneline --decorate --graph --all
+	git log --oneline --decorate --graph --all $argv
 end
--- home/.config/fish/functions/glo.fish
+++ home/.config/fish/functions/glo.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/s7/2ypm2j2j7m1_71jb9wx02tsh0000gq/T//fish.5gzCJu/glo.fish @ line 1
+# Defined in /var/folders/s7/2ypm2j2j7m1_71jb9wx02tsh0000gq/T//fish.HRVgeQ/glo.fish @ line 2
 function glo
-	git log --oneline --decorate --graph
+	git log --oneline --decorate --graph $argv
 end
