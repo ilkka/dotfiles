be810f190505164f8763302d7bbdddbd11f6cec0
Ilkka Poutanen <ilkka@ilkka.io>
2017-08-22 12:21:51 +0300

    Allow args for gco

--- home/.config/fish/functions/gco.fish
+++ home/.config/fish/functions/gco.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/s7/2ypm2j2j7m1_71jb9wx02tsh0000gq/T//fish.iVhiRf/gco.fish @ line 1
+# Defined in /var/folders/s7/2ypm2j2j7m1_71jb9wx02tsh0000gq/T//fish.kU6OvP/gco.fish @ line 2
 function gco
-	git commit -v
+	git commit -v $argv
 end
