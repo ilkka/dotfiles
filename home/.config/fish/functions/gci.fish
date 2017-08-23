f5f8d840f51b777f31d975610ec8438b09529d31
Ilkka Poutanen <ilkka@ilkka.io>
2017-08-23 10:38:42 +0300

    gco -> checkout, gci -> commit

---
+++ home/.config/fish/functions/gci.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function gci
+	git commit -v $argv
+end
--- home/.config/fish/functions/gco.fish
+++ home/.config/fish/functions/gco.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/s7/2ypm2j2j7m1_71jb9wx02tsh0000gq/T//fish.kU6OvP/gco.fish @ line 2
+# Defined in - @ line 2
 function gco
-	git commit -v $argv
+	git checkout $argv
 end
