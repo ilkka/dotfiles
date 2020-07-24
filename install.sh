2855b38ea95186d10febffdaccbd57c7adb9020e
Ilkka Poutanen <ilkka@ilkka.dev>
2020-07-24 10:33:28 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# This is the dotfiles install script for vs code remote dev containers (and
+# why not for other things, but written to work for that purpose)
+
+# yes it might not work for absolutely every scenario blah blah
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd $SCRIPT_DIR
+git submodule update --init
+
+HOMESHICK_REPOS=$HOME/.homesick/repos
+HOMESHICK_DIR=$HOMESHICK_REPOS/homeshick
+if [ ! -e $HOMESHICK_DIR ]; then
+  git clone https://github.com/andsens/homeshick.git $HOMESHICK_DIR
+fi
+. $HOMESHICK_DIR/homeshick.sh
+
+DOTFILES_DIR=$HOMESHICK_REPOS/dotfiles
+if [ ! -e $DOTFILES_DIR ]; then
+  ln -s $SCRIPT_DIR $DOTFILES_DIR
+fi
+
+homeshick link --force
+
+#vim: sw=2 sts=2 et
