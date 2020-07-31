ec54ca96570147b52f52d534f293e8e669b8b9e4
Ilkka Poutanen <ilkka@ilkka.dev>
2020-07-31 22:40:59 +0300

    Some configuration for the plantuml layer

--- home/.spacemacs.d/init.el
+++ home/.spacemacs.d/init.el
@@ -506,7 +506,9 @@ before packages are loaded."
     (add-to-list 'auto-mode-alist '("\\.beancount\\'" . beancount-mode))
     (add-hook 'beancount-mode-hook #'outline-minor-mode))
   (setq-default scroll-margin 4)
-  (when (file-exists-p "~/.spacemacs-local.el") (load "~/.spacemacs-local")))
+  (when (file-exists-p "~/.spacemacs-local.el") (load "~/.spacemacs-local"))
+  (setq plantuml-default-exec-mode 'jar)
+  (add-to-list 'auto-mode-alist '("\\.wsd\\'" . plantuml-mode)))
 
 (defun dotspacemacs/emacs-custom-settings ()
   "Emacs custom settings.
