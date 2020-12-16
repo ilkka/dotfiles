5ff0fa197847eb94d7150c7f5a1f830520692b54
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2020-12-16 14:33:02 +0200

    Emacs java layer

--- home/.spacemacs.d/init.el
+++ home/.spacemacs.d/init.el
@@ -49,11 +49,7 @@ This function should only modify configuration layer settings."
        prettier
        html
        elixir
-       ;; ----------------------------------------------------------------
-       ;; Example of useful layers you may want to use right away.
-       ;; Uncomment some layer names and press `SPC f e R' (Vim style) or
-       ;; `M-m f e R' (Emacs style) to install them.
-       ;; ----------------------------------------------------------------
+       java
        helm
        auto-completion
        ;; better-defaults
