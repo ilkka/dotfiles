31d209b5f2b693624066637364b3b9efee4a09f4
Ilkka Poutanen <ilkka@ilkka.dev>
2021-07-20 12:17:16 +0300

    Add more vsnip keymaps

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -183,6 +183,16 @@ return require('packer').startup(function()
             vim.api.nvim_set_keymap('s', '<C-j>', [[vsnip#expandable() ? '<Plug>(vsnip-expand)' : '<C-j>']], { expr = true })
             vim.api.nvim_set_keymap('i', '<C-l>', [[vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>']], { expr = true })
             vim.api.nvim_set_keymap('s', '<C-l>', [[vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>']], { expr = true })
+
+            vim.api.nvim_set_keymap('i', '<Tab>', [[vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>']], { expr = true })
+            vim.api.nvim_set_keymap('s', '<Tab>', [[vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>']], { expr = true })
+            vim.api.nvim_set_keymap('i', '<S-Tab>', [[vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>']], { expr = true })
+            vim.api.nvim_set_keymap('s', '<S-Tab>', [[vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>']], { expr = true })
+
+            vim.api.nvim_set_keymap('n', 's', [[<Plug>(vsnip-select-text)]], {})
+            vim.api.nvim_set_keymap('x', 's', [[<Plug>(vsnip-select-text)]], {})
+            vim.api.nvim_set_keymap('n', 'S', [[<Plug>(vsnip-cut-text)]], {})
+            vim.api.nvim_set_keymap('x', 'S', [[<Plug>(vsnip-cut-text)]], {})
         end,
         requires = {
             "hrsh7th/vim-vsnip-integ"
