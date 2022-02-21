778e5c5830f3677766bfa7c40157f68aae0b71ef
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2022-02-21 11:55:36 +0200

    doom emacs config

---
+++ dot_doom.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auth-sources
+   '(default "/home/ilkka/.emacs.d/.local/etc/authinfo.gpg" "~/.authinfo.gpg" "~/.authinfo")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
