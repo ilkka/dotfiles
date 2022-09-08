d3321aaf0f1888618bc454ba1598f33bee942f91
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2022-09-08 17:16:45 +0300

    Cannot rm without perms on parent

--- run_once_install-golang.sh
+++ run_once_install-golang.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -fsSLo /tmp/golang.tar.gz https://go.dev/dl/go1.19.1.linux-amd64.tar.gz
-rm -rf /usr/local/go
-sudo mkdir /usr/local/go && sudo chown $(id -u):$(id -g) /usr/local/go
-tar -C /usr/local -xf /tmp/golang.tar.gz
+sudo rm -rf /usr/local/go
+sudo mkdir /usr/local/go
+sudo tar -C /usr/local -xf /tmp/golang.tar.gz
 rm -f /tmp/golang.tar.gz
