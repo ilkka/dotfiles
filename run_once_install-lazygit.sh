f781408e27b500729b39715bf556e584421e9315
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2022-09-08 16:29:08 +0300

    Install lazygit

---
+++ run_once_install-lazygit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-35.]+')
+curl -Lo /tmp/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf /tmp/lazygit.tar.gz -C "$HOME/.local/bin" lazygit
+rm -f /tmp/lazygit.tar.gz
