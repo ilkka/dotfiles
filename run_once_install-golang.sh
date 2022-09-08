a4eb0d2aa4e31cea25e1b9906f8d7b2d2c0895d2
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2022-09-08 17:15:31 +0300

    Fix tar cmdline

--- run_once_install-golang.sh
+++ run_once_install-golang.sh
@@ -3,5 +3,5 @@
 curl -fsSLo /tmp/golang.tar.gz https://go.dev/dl/go1.19.1.linux-amd64.tar.gz
 rm -rf /usr/local/go
 sudo mkdir /usr/local/go && sudo chown $(id -u):$(id -g) /usr/local/go
-tar -C /usr/local -f /tmp/golang.tar.gz
+tar -C /usr/local -xf /tmp/golang.tar.gz
 rm -f /tmp/golang.tar.gz
