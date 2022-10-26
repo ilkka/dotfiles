3ff6f3c686ada19e724fe10c16adf6ea0e6d77f8
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2022-10-26 11:05:59 +0300

    emacs: play with font settings

--- dot_spacemacs.d/init.el
+++ dot_spacemacs.d/init.el
@@ -291,9 +291,11 @@ It should only modify the values of Spacemacs settings."
    ;; (default t)
    dotspacemacs-colorize-cursor-according-to-state t
 
-   ;; Default font or prioritized list of fonts.
-   dotspacemacs-default-font '("Fira Code Nerd Font Mono"
-                               :size 12.0
+   ;; Default font or prioritized list of fonts. The `:size' can be specified as
+   ;; a non-negative integer (pixel size), or a floating-point (point size).
+   ;; Point size is recommended, because it's device independent. (default 10.0)
+   dotspacemacs-default-font '("NotoSansMono Nerd Font"
+                               :size 10.0
                                :weight normal
                                :width normal)
 
