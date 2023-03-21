3abcdf9d60de0da0171427bb3a5193fd667093ca
Ilkka Poutanen <ilkka@ilkka.dev>
2023-03-21 15:06:05 +0200

    chezmoi: install fuse2 on arch

--- run_once_install-nice-tools.sh
+++ run_once_install-nice-tools.sh
@@ -4,7 +4,8 @@ if ! command -v exa &>/dev/null; then
     sudo apt install exa
   elif command -v pacman &>/dev/null; then
     sudo pacman -S yay
-    sudo yay -Syu exa bottom gdu
+    # fuse2 is needed for the neovim appimage
+    sudo yay -Syu exa bottom gdu fuse2
   else
     echo "Don't know how to install things on this platform" >&2
   fi
