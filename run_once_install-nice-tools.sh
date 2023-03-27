ed63ebc73be056c7c19bff82bdf20091198d3205
Ilkka Poutanen <ilkka@ilkka.dev>
2023-03-27 17:54:31 +0300

    Improve tools script
    - 'fd' on Ubuntu
    - libfuse2 on ubuntu
    - don't only check exa to see if it needs to run

--- run_once_install-nice-tools.sh
+++ run_once_install-nice-tools.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-if ! command -v exa &>/dev/null; then
-  if command -v apt &>/dev/null; then
-    sudo apt install exa
-  elif command -v pacman &>/dev/null; then
-    sudo pacman -S yay
-    # fuse2 is needed for the neovim appimage
-    sudo yay -Syu exa bottom gdu fuse2
-  else
-    echo "Don't know how to install things on this platform" >&2
-  fi
+if command -v apt &>/dev/null; then
+  # fuse2 is needed for the neovim appimage
+  sudo apt install exa fd-find libfuse2
+elif command -v pacman &>/dev/null; then
+  sudo pacman -S yay
+  # fuse2 is needed for the neovim appimage
+  sudo yay -Syu exa bottom gdu fuse2
+else
+  echo "Don't know how to install things on this platform" >&2
 fi
