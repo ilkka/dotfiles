a239ff38f41f0e91fdac656b9a5e56b347b8ce5b
Ilkka Poutanen <ilkka@ilkka.dev>
2023-05-18 14:19:16 +0300

    Finally trying to make chezmoi work in windows

--- run_once_install-golang.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -fsSLo /tmp/golang.tar.gz https://go.dev/dl/go1.19.1.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go
-sudo mkdir /usr/local/go
-sudo tar -C /usr/local -xf /tmp/golang.tar.gz
-rm -f /tmp/golang.tar.gz
