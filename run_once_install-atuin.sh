708509432cdbb9908327d837404341408efa9cb4
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-05-31 23:05:14 +0300

    Don't re-install atuin

--- run_once_install-atuin.sh
+++ run_once_install-atuin.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+if command -v atuin &>/dev/null; then
+  exit 0
+fi
 echo "Installing atuin (atuin.sh)"
 bash <(curl https://raw.githubusercontent.com/ellie/atuin/main/install.sh)
