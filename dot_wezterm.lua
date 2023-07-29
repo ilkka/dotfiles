7fc33699661d92c325a6e62dc1bc44ccc6df7ca8
Ilkka Poutanen <ilkka@ilkka.dev>
2023-07-29 11:10:34 +0300

    wezterm config

---
+++ dot_wezterm.lua
@@ -0,0 +1,22 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+--config.color_scheme = 'AdventureTime'
+
+config.default_prog = { 'C:/Program Files/PowerShell/7/pwsh.exe', '-l' }
+
+
+-- and finally, return the configuration to wezterm
+return config
