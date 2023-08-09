3bb2bfcdb08652e72b28d4bb132800e496d80666
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-08-09 10:15:23 +0300

    Use scoop version of npiperelay

--- dot_1password/executable_agent-bridge.sh
+++ dot_1password/executable_agent-bridge.sh
@@ -1,10 +1,13 @@
 export SSH_AUTH_SOCK="$HOME/.1password/agent.sock"
-ALREADY_RUNNING=$(ps -auxww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"; echo $?)
+ALREADY_RUNNING=$(
+	ps -auxww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"
+	echo $?
+)
 if [[ $ALREADY_RUNNING != "0" ]]; then
-  if [[ -S $SSH_AUTH_SOCK ]]; then
-    echo "removing previous socket..."
-    rm $SSH_AUTH_SOCK
-  fi
-  echo "starting SSH agent relay..."
-  (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"/mnt/c/Windows/System32/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
+	if [[ -S $SSH_AUTH_SOCK ]]; then
+		echo "removing previous socket..."
+		rm $SSH_AUTH_SOCK
+	fi
+	echo "starting SSH agent relay..."
+	(setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"/mnt/c/Users/ilkka/scoop/apps/npiperelay/current/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
 fi
