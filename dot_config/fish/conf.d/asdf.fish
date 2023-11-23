b6a19100ec1fb53e264dad9b12fcce482f827ff6
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-11-23 21:43:11 +0200

    Make asdf for from the mac location too

--- dot_config/fish/conf.d/asdf.fish
+++ dot_config/fish/conf.d/asdf.fish
@@ -1,4 +1,7 @@
 if test -f /home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.fish
     set -gx KERL_BUILD_DOCS yes
     source /home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.fish
+else if test -f /opt/homebrew/opt/asdf/libexec/asdf.fish
+    set -gx KERL_BUILD_DOCS yes
+    source /opt/homebrew/opt/asdf/libexec/asdf.fish
 end
