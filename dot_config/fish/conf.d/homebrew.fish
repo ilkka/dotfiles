a62aed1dbc8dab1547bb083b869d6e940f3d8976
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-11-23 21:45:27 +0200

    Homebrew and linuxbrew together

--- dot_config/fish/conf.d/homebrew.fish
+++ dot_config/fish/conf.d/homebrew.fish
@@ -1,3 +1,5 @@
 if test -x /home/linuxbrew/.linuxbrew/bin/brew
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+else if test -x /opt/homebrew/bin/brew
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 end
