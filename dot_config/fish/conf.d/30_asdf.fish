444bdbd8d1f704f8e903bf56a1e3c9846f50bc54
Ilkka Poutanen <ilkka@ilkka.dev>
2024-05-10 21:29:17 +0000

    Ok now hopefully finally fix asdf path

--- dot_config/fish/conf.d/30_asdf.fish
+++ dot_config/fish/conf.d/30_asdf.fish
@@ -4,7 +4,7 @@ if test -f /home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.fish
 else if test -f /opt/homebrew/opt/asdf/libexec/asdf.fish
     set -gx KERL_BUILD_DOCS yes
     source /opt/homebrew/opt/asdf/libexec/asdf.fish
-else if test -f $HOME/.asdd/asdf.fish
+else if test -f $HOME/.asdf/asdf.fish
     set -gx KERL_BUILD_DOCS yes
     source $HOME/.asdf/asdf.fish
 end 
