04850652e9472a6691fb493956a3c647bf590787
Ilkka Poutanen <ilkka.poutanen@61n.fi>
2024-12-09 12:21:39 +0200

    fish: allow 'bare' nix shell
    'bare' means don't require a shell.nix or default.nix.

--- dot_config/fish/functions/nixish.fish
+++ dot_config/fish/functions/nixish.fish
@@ -1,5 +1,8 @@
-if command -v podman
+if command -v podman &>/dev/null
     function nixish --wraps="podman" --description="Nix shell here"
-        podman run -it -v "$(pwd):/work" -w /work docker.io/nixos/nix nix-shell
+        argparse b/bare -- $argv
+        or return
+
+        podman run -it -v "$(pwd):/work" -w /work docker.io/nixos/nix $(if not set -ql _flag_bare; echo nix-shell; end)
     end
 end
